68c80ffef616c995b384167df8a846af45f580ba
dipesh.tiwari <dipesh.tiwari@ranosys.com>
2018-05-02 11:26:03 +0530

    index.js updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var path = require('path');
 var morgan = require('morgan'); // log requests to the console (express4)
 var bodyParser = require('body-parser'); // pull information from HTML POST (express4)
 var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
-var cors = require('cors');
+// var cors = require('cors');
 
 var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' })
 
@@ -34,7 +34,7 @@ app.use(bodyParser.urlencoded({ 'extended': 'true' })); // parse application/x-w
 app.use(bodyParser.json()); // parse application/json
 app.use(bodyParser.json({ type: 'application/vnd.api+json' })); // parse application/vnd.api+json as json
 app.use(methodOverride());
-app.use(cors());
+// app.use(cors());
 
 app.post('/fileUpload', fileUpload.upload.single('image'), (req, res, next) => {
     res.status(200).json({ 'message': 'File uploaded successfully' });
